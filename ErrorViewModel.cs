1b4ea088f8f170b375f639d0d514d4998fb81355
alfrev2206-maker <alfrev2206@gmail.com>
2026-08-17 21:42:42 -0500

    Add files via upload

---
+++ ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Autenticacion_Roles.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
