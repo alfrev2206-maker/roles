b2526b44ffb9e2c4a70a31a3843892ee82e91f7e
alfrev2206-maker <alfrev2206@gmail.com>
2026-08-17 21:42:18 -0500

    Add files via upload

---
+++ AccesoController.cs
@@ -0,0 +1,26 @@
+﻿using Autenticacion_Roles.Data;
+using Autenticacion_Roles.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Autenticacion_Roles.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Usuario u)
+        {
+            DA_Logica da = new DA_Logica();
+            var usuario = da.validarUsuario(u.Correo,u.Clave);
+            if (usuario != null) {
+                return RedirectToAction("Index", "Home");
+            }
+            return View();
+        }
+    }
+}
